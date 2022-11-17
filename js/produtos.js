ad0e41038563784e9eba6b014406fc9d75efe9ec
zeotoni <zeotoni666@gmail.com>
2022-11-16 22:44:08 -0300

    excluir produto

--- js/produtos.js
+++ js/produtos.js
@@ -26,7 +26,7 @@ const exibeCards = ()=> {
             cardNovo.innerHTML =  
             `<div class="card">
                 <div class="card__icons">
-                    <img src="../assets/img/botao-excluir.svg" alt="Ícone de lixeira" class="card__icons-img" data-tipo="excluir-produto">
+                    <img src="../assets/img/botao-excluir.svg" onclick="excluiProduto('${item.id}','${item.titulo}')" alt="Ícone de lixeira" class="card__icons-img" data-tipo="excluir-produto">
                     <img src="../assets/img/botao-editar.svg" alt="Ícone de lápis" class="card__icons-img">
                 </div>
                 <img src="${item.img}" alt="${item.alt}" class="card__img">
@@ -34,7 +34,6 @@ const exibeCards = ()=> {
                 <span class="card__preco">${item.preco}</span>
                 <a href="../html/produto-descricao.html?id=${item.id}" class="card__link" data-tipo="link-produto">Ver Produto</a>
             </div>`
-           
             document.querySelector('[data-tipo="produtos"]').appendChild(cardNovo)
         });
     })
@@ -43,3 +42,14 @@ const exibeCards = ()=> {
 
 exibeCards();
 
+function excluiProduto(id, nome) {
+    console.log(id)
+    alert(`Deseja excluir o produto:  ${nome} ?`)
+    // alert(`Deseja excluir o produto ${id} ?`)
+    // fetch('https://db-geek.herokuapp.com/produtos/'+id, {
+    //     method: 'DELETE'
+    // })
+    // .then(res => res.json())
+    // .then(res => console.log(res))
+    // .catch(error => console.log(error)); 
+}
